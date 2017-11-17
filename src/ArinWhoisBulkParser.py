49cce52203c43f421c428d2f59a15baa8c60dfa8
Arjan van der Veen <avdrveen@icense.nl>
2017-11-17 23:45:01 +0100

    output format added: now irr and json as first call parameter, then the file name(s)

--- src/ArinWhoisBulkParser.py
+++ src/ArinWhoisBulkParser.py
@@ -1,6 +1,8 @@
 '''
 Created on Nov 9, 2017
 
+Usage: call script <format: 'json' or 'irr'> <xml source>
+
 @author: arjan
 @ook in geharkt: job
 '''
@@ -8,6 +10,14 @@ Created on Nov 9, 2017
 import fileinput
 import netaddr
 import re
+import datetime
+import json
+import sys
+
+outputFormat = sys.argv[1]
+if not (outputFormat == 'json' or 'irr'):
+    print 'unknown output format'
+    exit(2)
 
 pCidrLength = re.compile('<cidrLenth>(.+?)<\/cidrLenth>')
 pStartAddress = re.compile('<startAddress>(.+?)<\/startAddress>\s*</netBlock>')
@@ -16,7 +26,9 @@ pOriginAS = re.compile('<originAS>(.+?)<\/originAS>')
 record = ''
 linenr = 0
 
-for line in fileinput.input():
+v4records = []
+v6records = []
+for line in fileinput.input(sys.argv[2:]):
     linenr += 1
     record += line
     if line == '</net>\n':
@@ -32,12 +44,32 @@ for line in fileinput.input():
                     if int(mCidrLength[x]) > 48 and ':' in vAddress:
                         continue
                     pfx = netaddr.IPNetwork(vAddress + '/' + mCidrLength[x])
-                    if pfx.version == 4:
-                        print "route: %s" % str(pfx)
-                    else:
-                        print "route6: %s" % str(pfx)
-                    print "origin: AS%s" % vAS
-                    print "source: ARIN-WHOIS"
-                    print ""
+                    if outputFormat == 'irr':
+                        if pfx.version == 4:
+                            print "route: %s" % str(pfx)
+                        else:
+                            print "route6: %s" % str(pfx)
+                        print "origin: AS%s" % vAS
+                        print "source: ARIN-WHOIS"
+                        print ""
+                    elif outputFormat == 'json':
+                        d = {}
+                        d['prefix'] = str(pfx)
+                        d['originas'] = 'AS%s' % vAS
+                        if pfx.version == 4:
+                            v4records.append(d)
+                        else:
+                            v6records.append(d)
                     x += 1
         record = ''
+if outputFormat == 'json':
+    arin = {}
+    arin['generation_date'] = datetime.datetime.utcnow().isoformat()
+    arin['whois_records'] = {}
+    arin['whois_records']['v4'] = v4records
+    arin['whois_records']['v6'] = v6records
+    arin['json_schema'] = '0.0.2'
+    arin['source'] = 'ARIN-WHOIS'
+    arin['help'] = 'http://teamarin.net/2016/07/07/origin-as-an-easier-way-to-validate-letters-of-authority/'
+    print json.dumps(arin, sort_keys=True, indent=4)
+
